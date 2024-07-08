b421ed2a2b42fc5167c2dc02fe5fb09dc3f5369a
Kaih White <kaihgwhite@outlook.com>
2024-07-08 05:06:04 -0700

    first commit

---
+++ premake5.lua
@@ -0,0 +1,54 @@
+workspace "OurEngine"
+   configurations { "Debug", "Release" }
+   startproject "Engine"
+
+   -- Path to the Dependencies directory
+   IncludeDir = {}
+   IncludeDir["GLFW"] = "Dependencies/GLFW"  -- Base GLFW path
+   IncludeDir["glm"] = "Dependencies/glm"
+   IncludeDir["Vulkan"] = "Dependencies/VulkanSDK"
+
+project "Engine"
+   kind "ConsoleApp"
+   language "C++"
+   cppdialect "C++17"
+
+   targetdir "bin/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}"
+   objdir "bin-int/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}"
+
+   files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp", "%{prj.name}/src/**.c" }
+
+   -- Windows specific settings
+   filter "system:windows"
+      includedirs { IncludeDir["GLFW"] .. "/Windows/include", IncludeDir["glm"], IncludeDir["Vulkan"] .. "/Include"}
+      libdirs { IncludeDir["GLFW"] .. "/Windows/lib-vc2022", IncludeDir["Vulkan"] .. "/Lib"}
+      architecture "x64"
+      systemversion "latest"
+      defines { "PLATFORM_WINDOWS" }
+      links { "glfw3", "vulkan-1"}
+
+      
+   -- TODO MacOS specific settings
+   -- filter "system:macosx"
+   --    includedirs { IncludeDir["GLFW"] .. "/MacOS/include" }
+   --    libdirs { IncludeDir["GLFW"] .. "/MacOS/lib-arm64" }
+   --    links { "glfw3" }
+   --    architecture "arm64" -- or "x64" for Intel, "arm64" for M1 specifically, or "universal"
+   --    systemversion "latest"
+   --    defines { "PLATFORM_MACOS" }
+
+   -- TODO Linux specific settings
+
+      -- General settings for Debug and Release configurations
+   filter "configurations:Debug"
+   defines { "DEBUG" }
+   symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
+
+   filter {"system:windows", "configurations:Release"}
+      buildoptions "/MD"
+   filter {"system:windows", "configurations:Debug"}
+      buildoptions "/MDd"
